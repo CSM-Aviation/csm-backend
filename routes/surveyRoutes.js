77c93bbec4e66056322d05b6fefa91cb23893168
Goutham Reddy Kallem <gouthamreddykallem@Gouthams-MacBook-Air.local>
2024-12-17 22:07:19 -0800

    testimonials

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -6,7 +6,13 @@ const { authenticateToken } = require('../middleware/authMiddleware');
 // Public route for submitting surveys
 router.post('/', surveyController.submitSurvey);
 
+// Handle approval/rejection via email links
+router.get('/approve/:token', surveyController.handleApproval);
+router.get('/reject/:token', surveyController.handleApproval);
 
-router.get('/', surveyController.getSurveys);
+router.put('/:id/approval', authenticateToken, surveyController.updateApprovalStatus);
+
+router.get('/', surveyController.getApprovedSurveys);
+router.get('/all', surveyController.getAllSurveys);
 
 module.exports = router;
